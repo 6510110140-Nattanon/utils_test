6e4e7986f9e9baccb8180af46ff7175d9178048f
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-02-19 17:34:19 +0700

    test alternating

---
+++ hackerrank/alternating/tests/test_alternating.py
@@ -0,0 +1,25 @@
+from algo.alternating import alternatingCharacter 
+
+import unittest
+
+class TestAlternatingCharacter(unittest.TestCase):
+    def test_AAAA_in_alternating(self):
+        x = 'AAAA'
+        result = alternatingCharacter(x)
+        self.assertEqual(result,3)
+    def test_BBBBB_in_alternating(self):
+        x = 'BBBBB'
+        result = alternatingCharacter(x)
+        self.assertEqual(result,4)
+    def test_ABABABAB_in_alternating(self):
+        x = 'ABABABAB'
+        result = alternatingCharacter(x)
+        self.assertEqual(result,0)
+    def test_BABABA_in_alternating(self):
+        x = 'BABABA'
+        result = alternatingCharacter(x)
+        self.assertEqual(result,0)
+    def test_AAABBB_in_alternating(self):
+        x = 'AAABBB'
+        result = alternatingCharacter(x)
+        self.assertEqual(result,4)
