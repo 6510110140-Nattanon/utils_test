b8ac56ff1bba50c41d84d0003f809aef7126046d
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-02-19 17:18:34 +0700

    test FunnyString

---
+++ hackerrank/funny-string/tests/tests_funnystring.py
@@ -0,0 +1,25 @@
+from algo.funnystring import funnyString 
+
+import unittest
+
+class TestFunnyString(unittest.TestCase):
+    def test_acxz_in_FunnyString(self):
+        x = 'acxz'
+        result = funnyString(x)
+        self.assertEqual(result,'Funny')
+    def test_bcxz_in_FunnyString(self):
+        x = 'bcxz'
+        result = funnyString(x)
+        self.assertEqual(result,'Not Funny')
+    def test_yrzxrxskrtlpwpmtpxvowrxrpxq_in_FunnyString(self):
+        x = 'yrzxrxskrtlpwpmtpxvowrxrpxq'
+        result = funnyString(x)
+        self.assertEqual(result,'Not Funny')
+    def test_jkmsxzwrxzy_in_FunnyString(self):
+        x = 'jkmsxzwrxzy'
+        result = funnyString(x)
+        self.assertEqual(result,'Not Funny')
+    def test_ivvkxq_in_FunnyString(self):
+        x = 'ivvkxq'
+        result = funnyString(x)
+        self.assertEqual(result,'Not Funny')
